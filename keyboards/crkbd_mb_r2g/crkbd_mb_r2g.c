131d44ae3cf3c7de9d71b4936e537d7d3f8cb315
shurizzle <me@shurizzle.dev>
2022-07-26 02:13:45 +0200

    Better support for idle

--- keyboards/crkbd_mb_r2g/crkbd_mb_r2g.c
+++ keyboards/crkbd_mb_r2g/crkbd_mb_r2g.c
@@ -17,6 +17,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "crkbd_mb_r2g.h"
 
+bool matrix_scan_task(void);
+bool quantum_task(void);
+extern void protocol_post_task(void);
+extern void protocol_pre_init(void);
+extern void protocol_post_init(void);
+
 #ifdef RGB_MATRIX_ENABLE
 
 // Logical Layout
@@ -89,60 +95,167 @@ led_config_t g_led_config = { {
 
 bool is_keyboard_master(void) { return is_keyboard_left(); }
 
-void try_usb_wake_up(void) {
-  if (USB_DeviceState == DEVICE_STATE_Suspended &&
-      USB_Device_RemoteWakeupEnabled && suspend_wakeup_condition()) {
-    USB_Device_SendRemoteWakeup();
-    clear_keyboard();
+static bool _suspend_wakeup_condition(void) {
+  for (uint8_t r = 0; r < MATRIX_ROWS; r++) {
+    if (matrix_get_row(r))
+      return true;
+  }
+  return false;
+}
 
-#if USB_SUSPEND_WAKEUP_DELAY > 0
-    wait_ms(USB_SUSPEND_WAKEUP_DELAY);
+static void _keyboard_task(void) {
+  bool is_suspended =
+      is_keyboard_master() && USB_DeviceState != DEVICE_STATE_Configured;
+
+  if (is_suspended) {
+    matrix_power_up();
+  }
+  bool matrix_changed = matrix_scan_task();
+  (void)matrix_changed;
+  if (is_suspended) {
+    matrix_power_down();
+  }
+
+  quantum_task();
+
+#if defined(RGBLIGHT_ENABLE)
+  rgblight_task();
+#endif
+
+#ifdef LED_MATRIX_ENABLE
+  led_matrix_task();
+#endif
+#ifdef RGB_MATRIX_ENABLE
+  rgb_matrix_task();
+#endif
+
+#if defined(BACKLIGHT_ENABLE)
+#if defined(BACKLIGHT_PIN) || defined(BACKLIGHT_PINS)
+  backlight_task();
+#endif
+#endif
+
+#ifdef ENCODER_ENABLE
+  bool encoders_changed = encoder_read();
+  if (encoders_changed)
+    last_encoder_activity_trigger();
+#endif
+
+  if (is_suspended) {
+    if ((matrix_changed
+#ifdef ENCODER_ENABLE
+         || encoders_changed
+#endif
+         ) &&
+        USB_Device_RemoteWakeupEnabled && _suspend_wakeup_condition()) {
+      USB_Device_SendRemoteWakeup();
+    }
+  } else {
+#ifdef OLED_ENABLE
+    oled_task();
+#if OLED_TIMEOUT > 0
+    // Wake up oled if user is using those fabulous keys or spinning those
+    // encoders!
+    if (matrix_changed
+#ifdef ENCODER_ENABLE
+        || encoders_changed
+#endif
+    )
+      oled_on();
+#endif
+#endif
+
+#ifdef ST7565_ENABLE
+    st7565_task();
+#if ST7565_TIMEOUT > 0
+    // Wake up display if user is using those fabulous keys or spinning those
+    // encoders!
+    if (matrix_changed
+#ifdef ENCODER_ENABLE
+        || encoders_changed
+#endif
+    )
+      st7565_on();
+#endif
+#endif
+
+#ifdef MOUSEKEY_ENABLE
+    // mousekey repeat & acceleration
+    mousekey_task();
+#endif
+
+#ifdef PS2_MOUSE_ENABLE
+    ps2_mouse_task();
+#endif
+
+#ifdef POINTING_DEVICE_ENABLE
+    pointing_device_task();
+#endif
+
+#ifdef MIDI_ENABLE
+    midi_task();
+#endif
+
+#ifdef VELOCIKEY_ENABLE
+    if (velocikey_enabled()) {
+      velocikey_decelerate();
+    }
+#endif
+
+#ifdef JOYSTICK_ENABLE
+    joystick_task();
+#endif
+
+#ifdef DIGITIZER_ENABLE
+    digitizer_task();
+#endif
+
+#ifdef PROGRAMMABLE_BUTTON_ENABLE
+    programmable_button_send();
 #endif
   }
+
+  led_task();
 }
 
 static int prev_USB_DeviceState = DEVICE_STATE_Unattached;
 
 static void _protocol_pre_task(void) {
   if (is_keyboard_master()) {
-    if (USB_DeviceState == DEVICE_STATE_Suspended &&
-        prev_USB_DeviceState != DEVICE_STATE_Suspended) {
+    if (USB_DeviceState != DEVICE_STATE_Configured &&
+        prev_USB_DeviceState == DEVICE_STATE_Configured) {
       suspend_power_down();
-    } else if (USB_DeviceState != DEVICE_STATE_Suspended &&
-               prev_USB_DeviceState == DEVICE_STATE_Suspended) {
+      clear_keyboard();
+    } else if (USB_DeviceState == DEVICE_STATE_Configured &&
+               prev_USB_DeviceState != DEVICE_STATE_Configured) {
       suspend_wakeup_init();
     }
     prev_USB_DeviceState = USB_DeviceState;
-
-    try_usb_wake_up();
   }
 }
-
-extern void protocol_post_task(void);
 
 void protocol_task(void) {
   _protocol_pre_task();
 
-  keyboard_task();
+  _keyboard_task();
 
   protocol_post_task();
 }
 
-extern void protocol_pre_init(void);
-extern void protocol_post_init(void);
+void keyboard_post_init_kb(void) {
+  if (is_keyboard_master()) {
+    prev_USB_DeviceState = USB_DeviceState;
+
+    if (USB_DeviceState != DEVICE_STATE_Configured) {
+      suspend_power_down();
+    }
+  }
+
+  keyboard_post_init_user();
+}
 
 void protocol_init(void) {
   protocol_pre_init();
-
-  if (is_keyboard_master()) {
-    while (USB_DeviceState != DEVICE_STATE_Configured) {
-      USB_USBTask();
-
-      try_usb_wake_up();
-    }
-
-    prev_USB_DeviceState = USB_DeviceState;
-  }
 
   keyboard_init();
 
