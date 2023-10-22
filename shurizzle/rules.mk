2e2488309012eb3c77d502d0d63929fc27cfc9f4
shurizzle <me@shurizzle.dev>
2023-10-22 04:59:04 +0200

    chore: update qmk support

--- shurizzle/rules.mk
+++ shurizzle/rules.mk
@@ -2,6 +2,6 @@ MOUSEKEY_ENABLE = no     # Mouse keys
 VIA_ENABLE      = no     # Enable VIA
 RGBLIGHT_ENABLE = no     # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+OLED_DRIVER     = ssd1306
 LTO_ENABLE      = yes
 EXTRAFLAGS     += -flto
